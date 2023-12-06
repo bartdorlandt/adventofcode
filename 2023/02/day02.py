0f6d579f31e93cd0249bad477db0c4105882f866
Bart Dorlandt <bart.dorlandt@imperva.com>
2023-12-06 20:10:49 +0100

    Day2

---
+++ 2023/02/day02.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import re
+from pathlib import Path
+
+
+def split_info(line):
+    d = {}
+    game_number, game_data = line.split(":")
+    number = game_number.split()[1].strip()
+    for games in game_data.strip().split(";"):
+        for sets in games.split(", "):
+            s, color = sets.split()
+            if color in d:
+                if int(s) > d[color]:
+                    d[color] = int(s)
+            else:
+                d[color] = int(s)
+    return number, d
+
+
+def gen_dict(lines: list[str]) -> dict:
+    d = {}
+    for line in lines:
+        number, data = split_info(line)
+        d[number] = data
+    return d
+
+
+def which_games(d: dict, test_game: dict) -> int:
+    games_fit = []
+    for game_number, game_data in d.items():
+        if (
+                test_game["red"] >= game_data["red"] and
+                test_game["green"] >= game_data["green"] and
+                test_game["blue"] >= game_data["blue"]
+        ):
+            games_fit.append(int(game_number))
+    with open("games_fit.txt", "w") as f:
+        f.write("\n".join(map(str, games_fit)))
+    return sum(games_fit)
+
+
+def power_of_cubes(cubes: dict) -> int:
+    return cubes["red"] * cubes["green"] * cubes["blue"]
+
+
+if __name__ == "__main__":
+    # input_file = Path("test_input.txt")
+    input_file = Path("day02.txt")
+    lines = input_file.read_text().splitlines()
+    d = gen_dict(lines)
+    print(d)
+    test_game = {
+        "red": 12,
+        "green": 13,
+        "blue": 14
+    }
+    print(which_games(d, test_game))
+    print(sum([power_of_cubes(d[game]) for game in d]))
